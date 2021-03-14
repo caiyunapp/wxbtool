6f291d1628631c1d923f63b84f15c88a4d6d7b9d
mingli <mingli.yuan@gmail.com>
2021-03-14 23:58:47 +0800

    bump to 0.0.19

--- wxbtool/mdls/resunet.py
+++ wxbtool/mdls/resunet.py
@@ -57,10 +57,10 @@ class ModelSetting(Setting):
             2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
             2010, 2011, 2012, 2013, 2014,
         ]
-        # temporal scopes for test
-        self.years_test = [2015]
         # temporal scopes for evaluation
-        self.years_eval = [2016, 2017, 2018]
+        self.years_eval = [2015, 2016]
+        # temporal scopes for test
+        self.years_test = [2017, 2018]
 
 
 class ResUNetModel(Base2d):
--- wxbtool/norms/meanstd.py
+++ wxbtool/norms/meanstd.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 
 def norm_t2m(t2m):
@@ -26,3 +27,53 @@ def norm_z500(z500):
 
 def norm_z1000(z1000):
     return (z1000 - 736.8600307366787) / 1072.7004633440063
+
+
+def denorm_t2m(t2m):
+    return t2m * 21.219592501509624 + 278.5193277994792
+
+
+def denorm_t850(t850):
+    return t850 * 15.572175300640202 + 274.57741292317706
+
+
+def denorm_tau(tau):
+    return tau * 3096.4446045099244 + 6048.8221254006414
+
+
+def denorm_tisr(tisr):
+    return tisr * 1439848.7984975462 + 1074511.0673076923
+
+
+def denorm_tcc(tcc):
+    return tcc * 0.3626919709448507 + 0.6740332964139107
+
+
+def denorm_z500(z500):
+    return z500 * 3353.5545664452306 + 54117.3952323718
+
+
+def denorm_z1000(z1000):
+    return z1000 * 1072.7004633440063 + 736.8600307366787
+
+
+normalizors = {
+    't2m': norm_t2m,
+    't850': norm_t850,
+    'tau': norm_tau,
+    'tisr': norm_tisr,
+    'tcc': norm_tcc,
+    'z500': norm_z500,
+    'z1000': norm_z1000,
+}
+
+
+denormalizors = {
+    't2m': denorm_t2m,
+    't850': denorm_t850,
+    'tau': denorm_tau,
+    'tisr': denorm_tisr,
+    'tcc': denorm_tcc,
+    'z500': denorm_z500,
+    'z1000': denorm_z1000,
+}
