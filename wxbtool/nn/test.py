8bbddb894ecaaad2b13d649d9267fa4c01232a9e
mingli <mingli.yuan@gmail.com>
2021-03-14 11:22:32 +0800

    WIP: correct tester

--- wxbtool/nn/test.py
+++ wxbtool/nn/test.py
@@ -12,7 +12,7 @@ import numpy as np
 import torch as th
 from torch.utils.data import DataLoader
 
-from plot.plotter import plot
+from wxbtool.plot.plotter import plot
 
 rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
 resource.setrlimit(resource.RLIMIT_NOFILE, (2048, rlimit[1]))
@@ -39,8 +39,16 @@ early_stopping = 5
 best = np.inf
 count = 0
 
+mdm = None
+try:
+    mdm = importlib.import_module(opt.module, package=None)
+except ImportError as e:
+    print('failure when loading model')
+    exit(1)
+
+name = mdm.model.setting.name
 time_str = arrow.now().format('YYYYMMDD_HHmmss')
-model_path = Path(f'./{name}-{time_str}')
+model_path = Path(f'./trains/{name}-{time_str}')
 model_path.mkdir(exist_ok=True)
 log_file = model_path / Path('train.log')
 
@@ -48,13 +56,6 @@ logging.basicConfig(level=logging.INFO, filename=log_file, filemode='w')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.info(str(opt))
-
-mdm = None
-try:
-    mdm = importlib.import_module('models.%s' % name, package=None)
-except ImportError as e:
-    logger.exception(e)
-    exit(1)
 
 
 def test_model(mdl):
@@ -72,7 +73,7 @@ def test_model(mdl):
     def test(mdl):
         mdl.eval()
 
-        dataloader = DataLoader(mdm.dataset_test, batch_size=opt.batch_size, shuffle=True, num_workers=opt.n_cpu)
+        dataloader = DataLoader(mdm.model.dataset_test, batch_size=opt.batch_size, shuffle=True, num_workers=opt.n_cpu)
         loss_per_epoch = 0.0
         rmse_per_epoch_t = 0.0
         inputs, targets, results = None, None, None
@@ -80,30 +81,25 @@ def test_model(mdl):
             inputs, targets = sample
 
             inputs = {
-                v: th.as_tensor(np.copy(inputs[v]), dtype=th.float32) for v in mdm.vars
+                v: th.as_tensor(np.copy(inputs[v]), dtype=th.float32) for v in mdm.setting.vars
             }
             targets = {
-                v: th.as_tensor(np.copy(targets[v]), dtype=th.float32) for v in mdm.vars
+                v: th.as_tensor(np.copy(targets[v]), dtype=th.float32) for v in mdm.setting.vars
             }
 
             if th.cuda.is_available():
                 inputs = {
-                    v: inputs[v].cuda() for v in mdm.vars
+                    v: inputs[v].cuda() for v in mdm.setting.vars
                 }
                 targets = {
-                    v: targets[v].cuda() for v in mdm.vars
+                    v: targets[v].cuda() for v in mdm.setting.vars
                 }
                 mdm.model.constant = mdm.model.constant.cuda()
-                if 'weight' in dir(mdm.model):
-                    mdm.model.weight = mdm.model.weight.cuda()
-                if 'ones' in dir(mdm.model):
-                    mdm.model.ones = mdm.model.ones.cuda()
-                if 'sphere' in dir(mdm.model):
-                    mdm.model.sphere = mdm.model.sphere.cuda()
+                mdm.model.weight = mdm.model.weight.cuda()
 
             with th.no_grad():
                 results = mdl(*[], **inputs)
-                loss = mdm.lossfun(inputs, results, targets)
+                loss = mdm.model.lossfun(inputs, results, targets)
                 logger.info(f'Step: {step + 1:03d} | Loss: {loss.item()}')
                 loss_per_epoch += loss.item() * list(results.values())[0].size()[0]
 
@@ -111,25 +107,25 @@ def test_model(mdl):
                 _, rst = mdm.model.get_results(**results)
                 tgt = tgt.detach().cpu().numpy().reshape(-1, 1, 32, 64)
                 rst = rst.detach().cpu().numpy().reshape(-1, 1, 32, 64)
-                eva.weight = eva.weight.reshape(1, 1, 32, 64)
-                eva.weight = eva.weight * (eva.weight > 0)
-                rmse = eva.weighted_rmse(rst, tgt)
+                mdm.model.eva.weight = mdm.model.eva.weight.reshape(1, 1, 32, 64)
+                mdm.model.eva.weight = mdm.model.eva.weight * (mdm.model.eva.weight > 0)
+                rmse = mdm.model.eva.weighted_rmse(rst, tgt)
                 logger.info(f'Step: {step + 1:03d} | Loss: {loss.item()} | Temperature RMSE: {rmse}')
                 rmse_per_epoch_t += np.nan_to_num(rmse * list(results.values())[0].size()[0])
 
-        rmse_total = rmse_per_epoch_t / mdm.test_size
-        logger.info(f'Test Loss: {loss_per_epoch / mdm.test_size}')
+        rmse_total = rmse_per_epoch_t / mdm.model.test_size
+        logger.info(f'Test Loss: {loss_per_epoch / mdm.model.test_size}')
         logger.info(f'Test RMSE: {rmse_total}')
 
         vars_in, _ = mdm.model.get_inputs(**inputs)
-        for bas, var in enumerate(mdm.vars_in):
-            for ix in range(mdm.input_span):
+        for bas, var in enumerate(mdm.model.vars_in):
+            for ix in range(mdm.setting.input_span):
                 img = vars_in[var][0, ix].detach().cpu().numpy().reshape(32, 64)
                 plot(var, open('%s_inp_%d.png' % (var, ix), mode='wb'), img)
 
         vars_fc, _ = mdm.model.get_results(**results)
         vars_tg, _ = mdm.model.get_targets(**targets)
-        for bas, var in enumerate(mdm.vars_out):
+        for bas, var in enumerate(mdm.setting.vars_out):
             fcst = vars_fc[var][0].detach().cpu().numpy().reshape(32, 64)
             tgrt = vars_tg[var][0].detach().cpu().numpy().reshape(32, 64)
             plot(var, open('%s_fcs.png' % var, mode='wb'), fcst)
@@ -143,6 +139,6 @@ def test_model(mdl):
 
 if __name__ == '__main__':
     if mdm != None:
-        mdm.load_dataset('test')
+        mdm.model.load_dataset('test')
         test_model(mdm.model)
     print('Test Finished!')
