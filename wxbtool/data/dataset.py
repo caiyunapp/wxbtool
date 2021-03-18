6a96e451ec7a0e2a6dfcc737ed94515f474ce539
mingli <mingli.yuan@gmail.com>
2021-03-19 00:28:33 +0800

    fix shape of dumps

--- wxbtool/data/dataset.py
+++ wxbtool/data/dataset.py
@@ -155,14 +155,12 @@ class WxDataset(Dataset):
             del self.targets[var]
 
     def memmap(self, dumpdir):
-        with open('%s/shapes.json' % dumpdir) as fp:
-            shapes = json.load(fp)
-
         for var in self.vars:
             input_dump = '%s/input_%s.npy' % (dumpdir, var)
             target_dump = '%s/target_%s.npy' % (dumpdir, var)
-            self.inputs[var] = np.memmap(input_dump, dtype=np.float32, shape=tuple(shapes['input']), mode='r')
-            self.targets[var] = np.memmap(target_dump, dtype=np.float32, shape=tuple(shapes['target']), mode='r')
+
+            self.inputs[var] = np.memmap(input_dump, mode='r')
+            self.targets[var] = np.memmap(target_dump, mode='r')
 
     def __len__(self):
         return self.inputs[self.vars[0]].shape[0]
