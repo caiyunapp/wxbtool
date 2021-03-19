92ceeebf3c60643b23e9f9f78336f674ecd983e0
Mingli Yuan <mingli.yuan@gmail.com>
2021-03-19 09:23:18 +0800

    seperate profile and schema from setting

--- wxbtool/specs/profile/weyn.py
+++ wxbtool/specs/profile/weyn.py
@@ -28,15 +28,3 @@ class ProfileWeyn(Setting):
         self.vars_in = ['z500', 'z1000', 'tau', 't850', 't2m', 'tisr']
         # The code of variables in output
         self.vars_out = ['t850']
-
-        # temporal scopes for train
-        self.years_train = [
-            1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989,
-            1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
-            2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
-            2010, 2011, 2012, 2013, 2014,
-        ]
-        # temporal scopes for evaluation
-        self.years_eval = [2015, 2016]
-        # temporal scopes for test
-        self.years_test = [2017, 2018]
